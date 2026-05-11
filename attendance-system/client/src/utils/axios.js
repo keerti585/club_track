76cac467da70553dc6b79953f1f230398d68924c
keerti585 <keertiachalkar9@gmail.com>
2026-05-11 06:24:18 +0530

    remove 401 redirect interceptor

--- attendance-system/client/src/utils/axios.js
+++ attendance-system/client/src/utils/axios.js
@@ -20,16 +20,4 @@ api.interceptors.request.use(
     }
 )
 
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem('token')
-            localStorage.removeItem('user')
-            window.location.href = '/login'
-        }
-        return Promise.reject(error)
-    }
-)
-
 export default api
